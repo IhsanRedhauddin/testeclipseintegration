35c6540cd82699b47b66de1dc2ae7bc0777be376
Ihsan Redhauddin <19FTT1094@student.pb.edu.bn>
2023-01-31 16:15:11 +0800

    push1

--- testeclipseintegration/src/testeclipseintegration/UWU.java
+++ testeclipseintegration/src/testeclipseintegration/UWU.java
@@ -12,7 +12,7 @@ public class UWU {
 	public static void main(String[] args) {
 		
 		System.out.println();
-		ClassA classA = new ClassA("ABCDEFGHIJ" ) ;
+		ClassA classA = new ClassA("ABCDEF" ) ;
 		System.out.println(classA.getABC());
 		System.out.println("We test fetching");
 		System.out.println("Sending message");
