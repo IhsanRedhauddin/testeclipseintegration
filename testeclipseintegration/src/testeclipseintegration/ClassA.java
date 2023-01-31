b37c5bfe45b1ce9f8bd367f152a2319f355dd2f4
Ihsan Redhauddin <19FTT1094@student.pb.edu.bn>
2023-01-31 15:26:38 +0800

    Retry (doing abcdef)

--- testeclipseintegration/src/testeclipseintegration/ClassA.java
+++ testeclipseintegration/src/testeclipseintegration/ClassA.java
@@ -1,5 +1,24 @@
 package testeclipseintegration;
 
 public class ClassA {
-
+	
+	private String abc ;
+	
+	public ClassA(String abc) {
+		
+		this.abc = abc;
+		
+	}
+	
+	public String getABC() {
+		return abc;
+	}
+	
+	public void setABC(String abc) {
+		this.abc = abc ;
+	}
+	
+	
+	
+	
 }
--- testeclipseintegration/src/testeclipseintegration/UWU.java
+++ testeclipseintegration/src/testeclipseintegration/UWU.java
@@ -12,6 +12,8 @@ public class UWU {
 	public static void main(String[] args) {
 		
 		System.out.println();
+		ClassA classA = new CLassA("ABCDEF" ) ;
+		System.out.println(classA.getABC());
 		
 	}
 	
